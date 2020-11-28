31983667b222b7a300c1242e0a300848a70c5a45
Joseph C Wang <joequant@gmail.com>
2020-11-28 21:20:34 +0800

    checkpoint

--- src/trader/strategy/test.py
+++ src/trader/strategy/test.py
@@ -1,11 +1,25 @@
 from trader.strategy import Strategy
+import trader.transform
 
-def TestStrategy(Strategy):
+class TestStrategy(Strategy):
     def __init__(self):
+        super().__init__()
         self.reflevel = None
-    def run(self, data, portfolio):
-        for index, row in df.iterrows():
-            if row['lastPrice'] != 0:
-                if self.reflevel is not None:
-                    self.reflevel = row['lastPrice']
+    def run(self, df, portfolio):
+        df = trader.transform.last_price(df)
+        for row in df.itertuples():
+            symbol = row[1]
+            time = row[2]
+            price =  row[3]
+            if self.reflevel is None:
+                self.reflevel = price
+            if price > 1.005 * self.reflevel:
+                portfolio.sell(symbol, portfolio.position(symbol) * 0.5,
+                               price, time)
+                self.reflevel = price
+            elif price < 0.9995 * self.reflevel:
+                portfolio.buy(symbol, portfolio.cash() * 0.5 / price,
+                              price, time)
+                self.reflevel = price
 
+                                   
