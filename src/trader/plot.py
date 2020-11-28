99c922e87a438d91a5cb3b8162aa133aa4d14b10
Joseph C Wang <joequant@gmail.com>
2020-11-29 05:56:31 +0800

    add plots and new design notes

--- src/trader/plot.py
+++ src/trader/plot.py
@@ -29,3 +29,19 @@ def plot_line(df, x, y):
 def pnl_plot(df, portfolio):
     return plot_test()
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def plot_animate_test():
+    fig, ax = plt.subplots()
+    x = np.arange(0, 2*np.pi, 0.01)
+    line, = ax.plot(x, np.sin(x))
+
+    def animate(i):
+        line.set_ydata(np.sin(x + i / 50))  # update the data.
+        return line,
+
+    ani = animation.FuncAnimation(
+        fig, animate, interval=20, blit=True, save_count=50)
+    plt.show()
