a96e8294357b4d9e8eb79aa9f877e569f48a53e4
Joseph C Wang <joequant@gmail.com>
2020-11-29 23:36:33 +0800

    add run-all.sh

---
+++ run-all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in data/*.csv ; do
+    echo $i
+    src/generate_report.py $i out templates/plots.mako templates/hello_world.mako templates/plot_pandas.mako templates/stats.mako templates/strategy.mako
+done
--- src/generate_report.py
+++ src/generate_report.py
@@ -7,12 +7,13 @@ import logging
 #print(sys.argv[1])
 #print(sys.argv[2])
 logging.info("reading " + sys.argv[1])
+base = os.path.basename(sys.argv[1]).rsplit('.', 1)[0]
 df = pandas.read_csv(sys.argv[1], index_col=1)
 logging.info("done reading")
 dir = sys.argv[2]
 for i in sys.argv[3:]:
     logging.info("reading template " + sys.argv[1])
     template = Template(filename=i)
-    with open(os.path.join(dir, os.path.basename(i).rsplit('.', 1)[0] + '.html'), "w") as f:
+    with open(os.path.join(dir, os.path.basename(i).rsplit('.', 1)[0] + "-" + base + '.html'), "w") as f:
         print(template.render(df=df), file=f)
     logging.info("finished template")
