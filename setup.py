c9f8eed816357589d1fc3d05bdefcd745076305a
Joseph C Wang <joequant@gmail.com>
2020-11-29 22:39:29 +0800

    add pip dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='trader',
+      version='0.0.1',
+      install_requires=[
+          'bokeh',
+          'pytz',
+          'mako',
+          'pandas',
+          'pandas-datareader'
+          ],
+      )
